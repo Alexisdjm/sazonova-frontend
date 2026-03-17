4764dd354cb5bb4b86d1aa5ae5b3fcb5e4766de3
Alexis Jimenez <Alexis.jm29@gmail.com>
2026-03-17 16:58:17 -0400

    feat: Implement the initial HeroBanner component, integrating new image assets and a scroll-down button.

---
+++ src/assets/exporting.js
@@ -0,0 +1,22 @@
+const images = {
+  // Icons
+  onion: require("./icons/onion.png"),
+  onion2: require("./icons/onion2.png"),
+  garlic: require("./icons/garlic.png"),
+  sazonovaLogoBeige: require("./icons/sazonova_logo_beige.png"),
+  sazonovaLogoRed: require("./icons/sazonova_logo_red.png"),
+  shoppingBag: require("./icons/shopping-bag.png"),
+  clickDown: require("./icons/Polygon2.png"),
+  // Images
+  ajo: require("./images/ajo.png"),
+  adobo: require("./images/adobo.png"),
+  image18: require("./images/pngwing.com (18).png"),
+  image19: require("./images/pngwing.com (19).png"),
+  image20: require("./images/pngwing.com (20).png"),
+  image21: require("./images/pngwing.com (21).png"),
+  image22: require("./images/pngwing.com (22).png"),
+  image38: require("./images/pngwing.com (38).png"),
+  image39: require("./images/pngwing.com (39).png"),
+};
+
+export default images;
