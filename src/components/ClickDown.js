33478ccb9129a23b3f60cf2893162c499428a834
Alexis Jimenez <Alexis.jm29@gmail.com>
2026-05-03 16:37:23 -0400

    feat: implement HeroBanner, Homepage, and ClickDown navigation components

--- src/components/ClickDown.js
+++ src/components/ClickDown.js
@@ -3,7 +3,7 @@ import images from "../assets/exporting";
 const ClickDown = () => {
   return (
     <>
-      <button onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })} className="absolute bottom-0 w-full flex flex-col items-center cursor-pointer animate-bounce mt-8">
+      <button onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })} className="absolute bottom-0 w-full flex flex-col items-center cursor-pointer animate-bounce mt-8 z-10">
         <p className="text-secondary-beige text-[12px] regular font-ubuntu">ver más</p>
         <img className="w-[50px] h-[50px]" src={images.clickDown} alt="ver más" />
       </button>
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -10,37 +10,37 @@ const HeroBanner = () => {
         <div className="w-full flex flex-col items-center">
           <h1
             data-text="el sabor"
-            className="isolate relative text-9xl font-medium font-sugo text-secondary-beige text-center uppercase tracking-[5px] before:content-[attr(data-text)] before:absolute before:inset-0 before:-z-10 before:bg-hero-gradient before:bg-fixed before:bg-clip-text before:text-transparent before:[-webkit-text-stroke:16px_transparent]"
+            className="isolate relative text-8xl md:text-9xl font-medium font-sugo text-secondary-beige text-center uppercase tracking-[5px] before:content-[attr(data-text)] before:absolute before:inset-0 before:-z-10 before:bg-hero-gradient before:bg-fixed before:bg-clip-text before:text-transparent before:[-webkit-text-stroke:16px_transparent]"
           >
             el sabor
           </h1>
           <h2
             data-text="que define"
-            className="isolate -mt-9 relative text-8xl font-sugo text-secondary-beige text-center uppercase tracking-[5px] before:content-[attr(data-text)] before:absolute before:inset-0 before:-z-10 before:bg-hero-gradient before:bg-fixed before:bg-clip-text before:text-transparent before:[-webkit-text-stroke:16px_transparent]"
+            className="isolate -mt-7 md:-mt-9 relative text-7xl md:text-8xl font-sugo text-secondary-beige text-center uppercase tracking-[5px] before:content-[attr(data-text)] before:absolute before:inset-0 before:-z-10 before:bg-hero-gradient before:bg-fixed before:bg-clip-text before:text-transparent before:[-webkit-text-stroke:16px_transparent]"
           >
             que define
           </h2>
           <h3
             data-text="tu cocina"
-            className="isolate -mt-10 relative text-9xl font-calling-heart text-secondary-beige text-center lowercase before:content-[attr(data-text)] before:absolute before:inset-0 before:-z-10 before:bg-hero-gradient before:bg-fixed before:bg-clip-text before:text-transparent before:[-webkit-text-stroke:12px_transparent]"
+            className="isolate -mt-8 md:-mt-10 relative text-8xl md:text-9xl font-calling-heart text-secondary-beige text-center lowercase before:content-[attr(data-text)] before:absolute before:inset-0 before:-z-10 before:bg-hero-gradient before:bg-fixed before:bg-clip-text before:text-transparent before:[-webkit-text-stroke:12px_transparent]"
           >
             tu cocina
           </h3>
         </div>
-        <p className="text-secondary-beige text-center w-[35%] text-[12px]">Ajo y Adobo: el origen del buen gusto. Simplificamos el arte de sazonar para que tú solo te preocupes por disfrutar del resultado.</p>
+        <p className="text-secondary-beige text-center w-[80%] md:w-[35%] text-[12px]">Ajo y Adobo: el origen del buen gusto. Simplificamos el arte de sazonar para que tú solo te preocupes por disfrutar del resultado.</p>
         <div className="absolute flex w-[50rem] justify-between flex-row">
           <img className="w-[175px] h-[350px] rotate-[12deg]" src={images.ajo} alt="" />
           <img className="w-[175px] h-[350px] rotate-[-12deg]" src={images.adobo} alt="" />
         </div>
         <ClickDown />
-        <img className="absolute rotate-[40deg] -right-[1%] -bottom-[8%] w-[85px] blur-[2px]" src={images.ajo} alt="" />
+        <img className="absolute rotate-[40deg] md:-right-[1%] -right-[1%] -bottom-[8%] w-[85px] blur-[2px]" src={images.ajo} alt="" />
         <img className="absolute rotate-[-30deg] -left-[3%] -bottom-[8%] w-[95px] blur-[2px]" src={images.adobo} alt="" />
-        <img className="absolute rotate-[40deg] left-[30%] top-[-12%] w-[80px] blur-[3px]" src={images.ajo} alt="" />
-        <img className="absolute rotate-[-45deg] right-[-2%] top-[20%] w-[95px] blur-[4px]" src={images.adobo} alt="" />
-        <img className="absolute rotate-[-90deg] right-[25%] bottom-[-12%] w-[85px] blur-[2px]" src={images.adobo} alt="" />
-        <img className="absolute rotate-[-85deg] right-[30%] top-[-12%] w-[85px] blur-[3px]" src={images.ajo} alt="" />
-        <img className="absolute rotate-[10deg] left-[-2%] top-[30%] w-[100px] blur-[4px]" src={images.adobo} alt="" />
-        <img className="absolute rotate-[55deg] left-[30%] bottom-[-12%] w-[90px] blur-[2px]" src={images.ajo} alt="" />
+        <img className="absolute rotate-[40deg] md:left-[30%] md:top-[-12%] left-[%] top-[-10%] w-[80px] blur-[3px]" src={images.ajo} alt="" />
+        <img className="absolute rotate-[-45deg] md:right-[-2%] right-[-5%] md:top-[20%] top-[60%] w-[95px] blur-[4px]" src={images.adobo} alt="" />
+        <img className="absolute rotate-[30deg] md:rotate-[-90deg] md:right-[25%] md:bottom-[-12%] left-[-10%] top-[20%] w-[85px] blur-[2px]" src={images.adobo} alt="" />
+        <img className="absolute md:rotate-[-85deg] rotate-[10deg] md:right-[30%] right-[-5%] md:top-[-12%] top-[15%] w-[85px] blur-[3px]" src={images.ajo} alt="" />
+        <img className="absolute rotate-[-25deg] md:left-[-2%] left-[-15%] md:top-[30%] top-[55%] w-[100px] blur-[4px]" src={images.adobo} alt="" />
+        <img className="absolute md:rotate-[55deg] rotate-[10deg] md:left-[30%] left-[40%] bottom-[-12%] w-[90px] blur-[2px]" src={images.ajo} alt="" />
       </div>
     </div>
   )
